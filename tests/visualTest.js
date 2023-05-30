33ba643b08780dbe2a030982693d1d011b409f5d
adarshTS <adarsh.ts8@gmail.com>
2023-05-30 12:40:10 +0530

    changed to local url

--- tests/visualTest.js
+++ tests/visualTest.js
@@ -4,23 +4,39 @@ const percySnapshot = require('@percy/selenium-webdriver');
 const { By } = require('selenium-webdriver');
 const BROWSERSTACK_USERNAME = process.env.BROWSERSTACK_USERNAME;
 const BROWSERSTACK_ACCESS_KEY = process.env.BROWSERSTACK_ACCESS_KEY;
+const browserstack = require('browserstack-local');
 
 var capabilities = {
 	'bstack:options' : {
 		"os" : "OS X",
 		"osVersion" : "Ventura",
-        "browserVersion" : "112.0",
-		"projectName" : "adS_Studio",
+		"browserVersion" : "16.0",
+		"projectName" : "bstack_sample",
+		"local" : "true",
 		"debug" : "true",
 		"networkLogs" : "true",
 		"seleniumVersion" : "3.14.0",
 		"userName" : process.env.BROWSERSTACK_USERNAME,
 		"accessKey" : process.env.BROWSERSTACK_ACCESS_KEY,
-		"localIdentifier" : process.env.BROWSERSTACK_LOCAL_IDENTIFIER
+		// "localIdentifier" : process.env.BROWSERSTACK_LOCAL_IDENTIFIER
 	},
-	"browserName" : "Chrome",
+	"browserName" : "Safari",
 }
 
+const bs_local = new browserstack.Local();
+const bs_local_args = { 'key': process.env.BROWSERSTACK_ACCESS_KEY, 'forceLocal': true };
+ 
+(async () => {
+  bs_local.start(bs_local_args, function(err) {
+      console.log(err);
+      if (!err){
+          console.log("Started BrowserStackLocal");
+          test (capabilities);
+      }
+      
+    });
+})();
+ 
   async function test (capabilities) {
     let driver = new webdriver.Builder()
     .usingServer(`http://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub`)
@@ -56,5 +72,12 @@ var capabilities = {
     await driver.quit();
   }
 
+
+ bs_local.stop(function() {
+    console.log("Stopped BrowserStackLocal");
+});
+
 }
-test (capabilities);
+
+
+
